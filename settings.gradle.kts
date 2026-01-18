265b5a94871293bcc82029f6414070d8b8e4ac75
ghost <nyklepro7755@gmail.com>
2026-01-18 22:43:55 +0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "V2rayAdGuard"
+
 include(":app")
 include(":core")
-include(":jni")
-include(":rust")
 include(":dns")
 include(":shared")
+// Временно закомментируй если мешают:
+// include(":rust")
+// include(":jni")
